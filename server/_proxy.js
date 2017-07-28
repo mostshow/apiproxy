2ee78bbd1d3b87f9fd95436de8e24b7b6a1a5e5d
lyndon <mostshow@users.noreply.github.com>
2017-07-28 11:03:26 +0800

    real time push data

--- server/_proxy.js
+++ server/_proxy.js
@@ -1,15 +1,16 @@
 
 import axios from 'axios'
-
+import querystring from 'querystring';
 
 function _proxy (opt ,response) {
-
 
     return new Promise(function (resolve) {
 			let l = opt.requestInfo.length;
 			let cookie, startTime = new Date().getTime();
 			opt.requestInfo.map( x => {
-				axios(x).then(function(res){
+				axios.post(x.url, querystring.stringify(x.data), {headers: {
+					Cookie: opt.cookie ? opt.cookie : ''
+				}}).then(function(res){
 					handle(x,res)
 				}).catch(function(err){
 					handle(x,err)
@@ -17,27 +18,29 @@ function _proxy (opt ,response) {
 			})
 
 			function handle(x,res){
-
 				l--;
 				let name = x.name;
 				let result = {};
-				if(res.errno){
-
-					result[name] ={
-						"errno" : res.errno,
-						"code" : res.code
+				let separator = '|qgz|';
+				console.log(res)
+				let statusText = res.statusText || res.response.statusText;
+				let status = res.status || res.response.status;
+				if(res.errno || !res.status || status !== 200){
+					result ={
+						"errno" : statusText,
+						"code" : status,
+						"status" : status,
+						"name" : name
 					} 
-					console.log(name + ':'+ (new Date().getTime() - startTime )) + 'ms';
+    				console.log('error') 
+					console.log(name + ':'+ (new Date().getTime() - startTime ) + 'ms');
 				}else{
 
-					result[name] = res.data;
-					// if(res.data.code == 90001){
-     //    				response.write(JSON.stringify(result) + ',');    
-					// }
-					//if(res.headers['set-cookie']) cookie = res.headers['set-cookie'];
-    				response.write( '"' + name + '"' + ':' + JSON.stringify(result[name]) +',');    
+					result = res.data;
+					result.name = name;
 					console.log(name + ':'+ (new Date().getTime() - startTime ) + 'ms');
 				}
+    			response.write( JSON.stringify(result) + separator);   
 			    !l&&resolve(result,cookie);
 
 			}
--- server/_router.js
+++ server/_router.js
@@ -5,32 +5,30 @@ import _proxy from './_proxy'
 
 const router = express.Router();
 
-router.post('/api/_proxy', (req, res, next) => {
-
+router.use('/api/proxy', (req, res, next) => {
     let cookie = req.get('Cookie');
-    res.writeHead(200, {
-        //'Content-Type': 'text/html; charset=utf-8',
-        'Content-Type': 'application/json ; charset=utf-8',
-        //'Content-Type': 'application/x-www-form-urlencoded',
-        //'Transfer-Encoding': 'chunked',
-        //'Vary': 'Accept-Encoding',
-        //'Set-Cookie': 'test=1;',
-        //'Connection': 'keep-alive'
-    })      
+    // res.writeHead(200, {
+    //     //'Content-Type': 'text/html; charset=utf-8',
+    //     'Content-Type': 'application/json ; charset=utf-8',
+    //     //'Content-Type': 'application/x-www-form-urlencoded',
+    //     //'Transfer-Encoding': 'chunked',
+    //     //'Vary': 'Accept-Encoding',
+    //     //'Set-Cookie': 'test=1;',
+    //     //'Connection': 'keep-alive'
+    // }) 
+       
     _proxy({
 
         requestInfo: req.body.requestInfo,
         cookie: cookie
 
     }, res).then(function(result, cookie){
-        console.log(result)
-
         //cookie&&res.append('Set-Cookie', cookie);
 
         res.end();    
 
     }).catch(function(){
-
+        console.log(123)
         res.send( JSON.stringify({
             code:-99,
             msg:'error!'
